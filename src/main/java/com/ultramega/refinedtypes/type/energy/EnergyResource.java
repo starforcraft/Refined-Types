69397593272788dd7196727ced478dbe7213f9a1
Starforcraft <starforcraft@yahoo.com>
2025-12-01 15:31:50 +0100

    Fixed energy, source and soul not being exported when fuzzy mode is on

--- src/main/java/com/ultramega/refinedtypes/type/energy/EnergyResource.java
+++ src/main/java/com/ultramega/refinedtypes/type/energy/EnergyResource.java
@@ -3,14 +3,16 @@ package com.ultramega.refinedtypes.type.energy;
 import com.ultramega.refinedtypes.registry.Types;
 import com.ultramega.refinedtypes.type.Type;
 
+import com.refinedmods.refinedstorage.api.resource.ResourceKey;
 import com.refinedmods.refinedstorage.common.Platform;
+import com.refinedmods.refinedstorage.common.api.support.resource.FuzzyModeNormalizer;
 import com.refinedmods.refinedstorage.common.api.support.resource.PlatformResourceKey;
 import com.refinedmods.refinedstorage.common.api.support.resource.ResourceTag;
 import com.refinedmods.refinedstorage.common.api.support.resource.ResourceType;
 
 import java.util.List;
 
-public record EnergyResource(Type type) implements PlatformResourceKey { //TODO: make an abstract class with the other Resource Types
+public record EnergyResource(Type type) implements PlatformResourceKey, FuzzyModeNormalizer { //TODO: make an abstract class with the other Resource Types
     public static EnergyResource createEnergyResource() {
         return new EnergyResource(Types.FE.get());
     }
@@ -28,6 +30,11 @@ public record EnergyResource(Type type) implements PlatformResourceKey { //TODO:
     @Override
     public List<ResourceTag> getTags() {
         return List.of();
+    }
+
+    @Override
+    public ResourceKey normalize() {
+        return new EnergyResource(this.type);
     }
 
     @Override
--- src/main/java/com/ultramega/refinedtypes/type/soul/SoulResource.java
+++ src/main/java/com/ultramega/refinedtypes/type/soul/SoulResource.java
@@ -3,13 +3,15 @@ package com.ultramega.refinedtypes.type.soul;
 import com.ultramega.refinedtypes.registry.Types;
 import com.ultramega.refinedtypes.type.Type;
 
+import com.refinedmods.refinedstorage.api.resource.ResourceKey;
+import com.refinedmods.refinedstorage.common.api.support.resource.FuzzyModeNormalizer;
 import com.refinedmods.refinedstorage.common.api.support.resource.PlatformResourceKey;
 import com.refinedmods.refinedstorage.common.api.support.resource.ResourceTag;
 import com.refinedmods.refinedstorage.common.api.support.resource.ResourceType;
 
 import java.util.List;
 
-public record SoulResource(Type type) implements PlatformResourceKey {
+public record SoulResource(Type type) implements PlatformResourceKey, FuzzyModeNormalizer {
     public static SoulResource createSoulResource() {
         return new SoulResource(Types.SOUL.get());
     }
@@ -27,6 +29,11 @@ public record SoulResource(Type type) implements PlatformResourceKey {
     @Override
     public List<ResourceTag> getTags() {
         return List.of();
+    }
+
+    @Override
+    public ResourceKey normalize() {
+        return new SoulResource(this.type);
     }
 
     @Override
